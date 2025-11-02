8c08f264b87e35b378dff716f082059bf889aedf
Enzo Villalba <enzo.villalba@epitech.eu>
2025-11-02 14:10:08 +0100

    add

---
+++ handle/handle_basic_array.c
@@ -0,0 +1,99 @@
+/*
+** EPITECH PROJECT, 2025
+** G-PSU-100-LYN-1-1-myls-7
+** File description:
+** handle_l_array.c
+*/
+
+#include "../include/my.h"
+
+char **create_l_array(file_data_t *file_data)
+{
+    char **array;
+
+    array = malloc(sizeof(char *) * 8);
+    array[0] = file_data->type_perm;
+    array[1] = file_data->hard_link;
+    array[2] = file_data->user_name;
+    array[3] = file_data->group_name;
+    array[4] = file_data->size;
+    array[5] = file_data->time;
+    array[6] = file_data->name;
+    array[7] = NULL;
+    return array;
+}
+
+static char **process_entry(struct dirent *entry, file_data_t *file_data,
+    flags_t *flag, has_file_flag_t *has_file_flag)
+{
+    struct stat file_stats;
+    char *fp;
+
+    fp = malloc(my_strlen(has_file_flag->path) + my_strlen(entry->d_name) + 2);
+    if (fp == NULL)
+        return NULL;
+    my_strcpy(fp, has_file_flag->path);
+    if (has_file_flag->path[my_strlen(has_file_flag->path) - 1] != '/')
+        my_strcat(fp, "/");
+    my_strcat(fp, entry->d_name);
+    if (entry->d_name[0] != '.' || flag->a_flags == 1) {
+        if (stat(fp, &file_stats) == 0)
+            flag->blocks += file_stats.st_blocks;
+    }
+    get_file_info(entry, file_data, has_file_flag->path);
+    free(fp);
+    return create_l_array(file_data);
+}
+
+static char ***dir_to_l_array(file_data_t *file_data,
+    int array_len, flags_t *flag, has_file_flag_t *has_file_flag)
+{
+    char ***array;
+    struct dirent *entry;
+    DIR *dir_stream = opendir(has_file_flag->path);
+
+    array = malloc(sizeof(char **) * (array_len + 1));
+    if (dir_stream == NULL || array == NULL) {
+        perror("my_ls: cannot open directory or allocate memory");
+        return NULL;
+    }
+    entry = readdir(dir_stream);
+    for (int i = 0; entry != NULL && i < array_len; i++) {
+        array[i] = process_entry(entry, file_data, flag, has_file_flag);
+        entry = readdir(dir_stream);
+    }
+    array[array_len] = NULL;
+    closedir(dir_stream);
+    return array;
+}
+
+char ***sort_l_array(char ***array, int len_array)
+{
+    int j;
+    char **elem;
+
+    for (int i = 1; i < len_array; i++) {
+        elem = array[i];
+        j = i - 1;
+        while (j >= 0 && my_strcmp(array[j][6], elem[6]) > 0) {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = elem;
+    }
+    return array;
+}
+
+char ***handle_basic_array(file_data_t *file_data, flags_t *flag,
+    has_file_flag_t *has_file_flag)
+{
+    int array_len = dir_len(has_file_flag);
+    char ***array;
+
+    if (array_len == -1)
+        return NULL;
+    array = dir_to_l_array(file_data, array_len, flag, has_file_flag);
+    if (array == NULL)
+        return NULL;
+    return sort_l_array(array, array_len);
+}
---
+++ utils/dir_len.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2025
+** G-PSU-100-LYN-1-1-myls-7
+** File description:
+** dir_len.c
+*/
+
+#include "../include/my.h"
+#include <errno.h>
+#include <string.h>
+
+void print_error(const char *path)
+{
+    my_putstr_err("my_ls: cannot access '");
+    my_putstr_err(path);
+    my_putstr_err("': ");
+    my_putstr_err(strerror(errno));
+    my_putstr_err("\n");
+}
+
+int dir_len(has_file_flag_t *has_file_flag)
+{
+    int array_len = 0;
+    struct dirent *entry;
+    DIR *dir_stream = opendir(has_file_flag->path);
+
+    if (dir_stream == NULL) {
+        print_error(has_file_flag->path);
+        return -1;
+    }
+    entry = readdir(dir_stream);
+    while (entry != NULL) {
+        array_len++;
+        entry = readdir(dir_stream);
+    }
+    closedir(dir_stream);
+    return array_len;
+}
