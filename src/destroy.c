e7b53f9be1d70fe9612db044f6d9f76517aecb4c
Enzo Villalba <enzo.villalba@epitech.eu>
2025-11-12 22:02:54 +0100

    add

---
+++ src/destroy.c
@@ -0,0 +1,70 @@
+/*
+** EPITECH PROJECT, 2025
+** GraphicalC
+** File description:
+** destroy.c
+*/
+
+#include "../include/my.h"
+
+static void destroy_window(game_t *game)
+{
+    if (game->window)
+        sfRenderWindow_destroy(game->window);
+}
+
+static void destroy_background(game_t *game)
+{
+    if (game->background_texture)
+        sfTexture_destroy(game->background_texture);
+    if (game->background_sprite)
+        sfSprite_destroy(game->background_sprite);
+}
+
+void static destroy_duck(game_t *game)
+{
+    if (game->duck_sprite) {
+        sfSprite_destroy(game->duck_sprite);
+        game->duck_sprite = NULL;
+    }
+    if (game->duck_texture) {
+        sfTexture_destroy(game->duck_texture);
+        game->duck_texture = NULL;
+    }
+}
+
+static void destroy_clock(game_t *game)
+{
+    if (game->clock)
+        sfClock_destroy(game->clock);
+}
+
+static void destroy_btn(game_t *game)
+{
+    if (game->start_btn_sprite) {
+        sfSprite_destroy(game->start_btn_sprite);
+        game->start_btn_sprite = NULL;
+    }
+    if (game->start_btn_texture) {
+        sfTexture_destroy(game->start_btn_texture);
+        game->start_btn_texture = NULL;
+    }
+    if (game->exit_btn_sprite) {
+        sfSprite_destroy(game->exit_btn_sprite);
+        game->exit_btn_sprite = NULL;
+    }
+    if (game->exit_btn_texture) {
+        sfTexture_destroy(game->exit_btn_texture);
+        game->exit_btn_texture = NULL;
+    }
+}
+
+void destroy_game(game_t *game)
+{
+    destroy_window(game);
+    destroy_background(game);
+    destroy_duck(game);
+    destroy_btn(game);
+    destroy_clock(game);
+    free(game);
+}
---
+++ src/sprite_texture/duck.c
@@ -0,0 +1,94 @@
+/*
+** EPITECH PROJECT, 2025
+** GraphicalC
+** File description:
+** duck.c
+*/
+
+#include "../../include/my.h"
+
+bool create_duck(game_t *game)
+{
+    char *path = "assets/images/duck.png";
+    sfVector2f offset;
+
+    srand(time(NULL));
+    offset.x = - DUCK_WIDTH / DUCK_STATES;
+    offset.y = rand() % (GAME_HEIGHT - DUCK_HEIGHT);
+    game->duck_texture = sfTexture_createSrgbFromFile(path, NULL);
+    if (!game->duck_texture)
+        return false;
+    game->duck_sprite = sfSprite_create();
+    if (!game->duck_sprite)
+        return false;
+    game->duck_rect.height = DUCK_HEIGHT;
+    game->duck_rect.width = DUCK_WIDTH / DUCK_STATES;
+    game->duck_rect.left = 0;
+    game->duck_rect.top = 0;
+    sfSprite_move(game->duck_sprite, offset);
+    sfSprite_setTexture(game->duck_sprite, game->duck_texture, sfTrue);
+    sfSprite_setTextureRect(game->duck_sprite, game->duck_rect);
+    return true;
+}
+
+static void refresh_duck_sprite(game_t *game)
+{
+    float seconds = get_elapsed_time(game);
+
+    if (game->duck_sprite == NULL)
+        return;
+    if (seconds >= REFRESH_SECONDS) {
+        game->duck_rect.left += DUCK_WIDTH / DUCK_STATES;
+        if (game->duck_rect.left >= DUCK_WIDTH)
+            game->duck_rect.left = 0;
+        sfSprite_setTextureRect(game->duck_sprite, game->duck_rect);
+        sfClock_restart(game->clock);
+    }
+}
+
+static void change_speed(game_t *game)
+{
+    if (!(game->score % 5) && game->duck_speed != 6)
+        game->duck_speed += 0.5;
+}
+
+static void reset_duck(game_t *game)
+{
+    sfVector2f offset;
+
+    offset.x = - DUCK_WIDTH / DUCK_STATES;
+    offset.y = rand() % (GAME_HEIGHT - DUCK_HEIGHT);
+    sfSprite_setPosition(game->duck_sprite, offset);
+    game->duck_rect.left = 0;
+    sfSprite_setTextureRect(game->duck_sprite, game->duck_rect);
+}
+
+static void move_duck(game_t *game)
+{
+    sfVector2f offset = {game->duck_speed, 0.0};
+
+    if (game->duck_sprite == NULL)
+        return;
+    sfSprite_move(game->duck_sprite, offset);
+}
+
+void handle_duck(game_t *game)
+{
+    if (game->duck_sprite == NULL)
+        return;
+    refresh_duck_sprite(game);
+    move_duck(game);
+    // printf("score: %i, speed: %.1f\n", game->score, game->duck_speed);
+    printf("x: %i, y: %.1f\n", game->duck_pos.x, game->duck_pos.y);
+    if (game->mouse_clicked == true
+        && game->mouse_pos.x >= game->duck_pos.x
+        && game->mouse_pos.x <= game->duck_pos.x + DUCK_WIDTH / DUCK_STATES
+        && game->mouse_pos.y >= game->duck_pos.y
+        && game->mouse_pos.y <= game->duck_pos.y + DUCK_HEIGHT) {
+        reset_duck(game);
+        game->score += 1;
+        change_speed(game);
+    }
+    if (game->duck_pos.x == 800)
+        game->state = START_MENU;
+}
