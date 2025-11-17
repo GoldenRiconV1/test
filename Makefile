e840e5396580f54ea828906526dc8c8bef197dcd
Enzo Villalba <enzo.villalba@epitech.eu>
2025-11-17 23:25:52 +0100

    add

---
+++ Makefile
@@ -0,0 +1,31 @@
+##
+## EPITECH PROJECT, 2025
+## G-PSU-100-LYN-1-1-mysudo-39
+## File description:
+## Makefile
+##
+
+SRC	=	src/main.c	\
+		src/parser.c	\
+		src/utils/init.c	\
+
+OBJ	=	$(SRC:.c=.o)
+
+CC = epiclang
+CFLAGS += -g3
+NAME =	my_sudo
+
+all:	$(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) -o $(NAME) $(OBJ)
+
+clean:
+	rm -f $(OBJ)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ src/parser.c
@@ -0,0 +1,96 @@
+/*
+** EPITECH PROJECT, 2025
+** G-PSU-100-LYN-1-1-mysudo-39
+** File description:
+** parser.c
+*/
+
+#include "../include/include.h"
+
+static void u_flag(data_t *data, int i)
+{
+    if (strcmp(data->argv[1], "-u") == 0
+        && data->flags != U_AND_G_FLAG && i != 1) {
+        data->command[i - 3] = data->argv[i];
+        data->user_name = data->argv[2];
+        data->command[data->argc - 2] = NULL;
+        data->flags = U_FLAG;
+    }
+}
+
+static void g_flag(data_t *data, int i)
+{
+    if (strcmp(data->argv[1], "-g") == 0
+        && data->flags != U_AND_G_FLAG && i != 1) {
+        data->command[i - 3] = data->argv[i];
+        data->groupe_name = data->argv[2];
+        data->command[data->argc - 2] = NULL;
+        data->flags = G_FLAG;
+    }
+}
+
+static void u_and_g_flag(data_t *data, int i)
+{
+    if ((strcmp(data->argv[1], "-u") == 0 && strcmp(data->argv[3], "-g") == 0)
+        && i != 1) {
+        data->command[i - 5] = data->argv[i];
+        data->user_name = data->argv[2];
+        data->groupe_name = data->argv[4];
+        data->command[data->argc - 5] = NULL;
+        data->flags = U_AND_G_FLAG;
+    }
+    if ((strcmp(data->argv[1], "-g") == 0 && strcmp(data->argv[3], "-u") == 0)
+        && i != 1) {
+        data->command[i - 5] = data->argv[i];
+        data->user_name = data->argv[4];
+        data->groupe_name = data->argv[2];
+        data->command[data->argc - 5] = NULL;
+        data->flags = U_AND_G_FLAG;
+    }
+}
+
+static void empty_flag(data_t *data, int i)
+{
+    if (data->flags == EMPTY) {
+        data->command[i - 1] = data->argv[i];
+        printf("-empty %s\n", data->command[i - 1]);
+        data->command[data->argc - 1] = NULL;
+    }
+}
+
+static void flags(data_t *data)
+{
+    int i = 1;
+
+    if (strcmp(data->argv[1], "-u") == 0 || strcmp(data->argv[1], "-g") == 0)
+        i = 2;
+    if ((strcmp(data->argv[1], "-u") == 0 && strcmp(data->argv[3], "-g") == 0)
+        || (strcmp(data->argv[1], "-g") == 0 && strcmp(data->argv[3], "-u") == 0))
+        i = 5;
+    while (i < data->argc) {
+        u_and_g_flag(data, i);
+        u_flag(data, i);
+        g_flag(data, i);
+        i++;
+    }
+}
+
+bool parser(data_t *data)
+{
+    int i = 0;
+
+    if (data->argc <= 1)
+        return false;
+    if (data->argc == 2 && strcmp(data->argv[1], "-h") == 0) {
+        printf("usage: ./my_sudo -h\n");
+        printf("usage: ./my_sudo [-ug] [command [args ...]]\n");
+        return true;
+    }
+    flags(data);
+    printf("command: %s\n", data->command[0]);
+    printf("option1: %s\n", data->command[1]);
+    printf("option2: %s\n", data->command[2]);
+    printf("user: %s\n", data->user_name);
+    printf("grp: %s\n", data->groupe_name);
+    return false;
+}
