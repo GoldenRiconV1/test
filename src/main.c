bcf5693eb8fe39f0a9a238769aaf28d9b65cd9dc
Enzo Villalba <enzo.villalba@epitech.eu>
2025-11-18 17:02:00 +0100

    add

---
+++ src/main.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2025
+** G-PSU-100-LYN-1-1-mysudo-39
+** File description:
+** main.c
+*/
+
+#include "../include/include.h"
+
+int main(int argc, char **argv)
+{
+    data_t *data = malloc(sizeof(data_t));
+
+    init(data, argc, argv);
+    if (!data)
+        return 84;
+    if (!parser(data))
+        return 84;
+    free(data->command);
+    free(data);
+    return 0;
+}
---
+++ src/utils/get_uid_from_file.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** G-PSU-100-LYN-1-1-mysudo-39
+** File description:
+** get_uid_from_file.c
+*/
+
+#include "../../include/include.h"
+
+void get_uid_from_file(data_t *data)
+{
+
+}
