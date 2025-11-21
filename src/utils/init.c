6a89ace787b84146676384535ecaa1bede20d01b
Enzo Villalba <enzo.villalba@epitech.eu>
2025-11-21 19:13:10 +0100

    add

---
+++ src/utils/init.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2025
+** G-PSU-100-LYN-1-1-mysudo-39
+** File description:
+** init.c
+*/
+
+#include "../../include/my.h"
+
+void init(data_t *data, int argc, char **argv)
+{
+    data->command = malloc(sizeof(char *) * argc);
+    data->flags = EMPTY;
+    data->argc = argc;
+    data->argv = argv;
+    data->user_name = NULL;
+    data->group_name = NULL;
+    data->uid = -2;
+    data->gid = -2;
+    data->put_password = NULL;
+}
