18fa6ef4b41a35bc407059c99b7c3867c8663d0a
Enzo Villalba <enzo.villalba@epitech.eu>
2025-11-21 20:53:53 +0100

    add

---
+++ src/utils/free_prog.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2025
+** G-PSU-100-LYN-1-1-mysudo-39
+** File description:
+** free_prog.c
+*/
+
+#include "../../include/my.h"
+
+void free_prog(data_t *data)
+{
+    if (!data)
+        return;
+    if (data->command)
+        free(data->command);
+    if (data->put_password)
+        free(data->put_password);
+    if (data->user_name)
+        free(data->user_name);
+    if (data->group_name)
+        free(data->group_name);
+    if (data->get_hashed_pass)
+        free(data->get_hashed_pass);
+    free(data);
+}
