8b39ca7414a9bb4bd891242b99013ebcb8118634
Enzo Villalba <enzo.villalba@epitech.eu>
2025-11-24 15:20:44 +0100

    add

---
+++ src/main.c
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2025
+** G-PSU-100-LYN-1-1-mysudo-39
+** File description:
+** main.c
+*/
+
+#include "../include/my.h"
+
+int main(int argc, char **argv)
+{
+    data_t *data = malloc(sizeof(data_t));
+
+    if (!data)
+        return 84;
+    init(data, argc, argv);
+    if (!parser(data)) {
+        free_prog(data);
+        return 84;
+    }
+    if (data->flags == H_FLAG) {
+        free_prog(data);
+        return 0;
+    }
+    if (!auth(data))
+        return 84;
+    if (!data->good_pass) {
+        free_prog(data);
+        return 84;
+    }
+    printf("good pass\n");
+    free_prog(data);
+    return 0;
+}
