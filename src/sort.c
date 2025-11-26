17006037a8d55654ea1138aa1f694600d67b7bc7
Enzo Villalba <enzo.villalba@epitech.eu>
2025-11-26 18:31:11 +0100

    add

---
+++ src/sort.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** G-CPE-110-LYN-1-1-organized-33
+** File description:
+** sort.c
+*/
+
+#include "./include/my.h"
+
+int sort(void *data, char **args)
+{
+    return 0;
+}
